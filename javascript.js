1e035d6067f59c8a8afcf1a231cc5489c9951563
jsonsDev <183668194+jsonsDev@users.noreply.github.com>
2025-10-05 17:34:46 +0700

    Add basic math operators.

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function add(firstNum, secondNum) {
+    const sum = firstNum + secondNum;
+    return sum;
+}
+
+function subtract(firstNum, secondNum) {
+    const difference = firstNum - secondNum;
+    return difference;
+}
+
+function multiply(firstNum, secondNum) {
+    const product = firstNum * secondNum;
+    return product;
+}
+
+function divide(firstNum, secondNum) {
+    const difference = firstNum / secondNum;
+    return difference;
+}
